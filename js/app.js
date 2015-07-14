42c972b353cd7bd197bc5f365b11498c6f461d54
Deepjyoti Khakhlary <deepjyoti941@gmail.com>
2015-07-14 23:57:03 +0530

    creating chart directive

--- js/app.js
+++ js/app.js
@@ -1,1 +1,16 @@
 var app = angular.module('MyApp', []);
+
+app.directive('chart', function() {
+  return {
+    replace: true,
+    transclude: true,
+    templateUrl: 'chart.html'
+  };
+});
+
+app.directive('datapoint', function() {
+  return {
+    replace: true,
+    template: '<circle cx="20" cy="20" r="4" stroke-width="3" fill="#ffffff" stroke="#5B90BF"/>'
+  }
+});
